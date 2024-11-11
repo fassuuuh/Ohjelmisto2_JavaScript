491c7bcca2d8f8970d9f11c3ab6a6c2a29b00f35
faizabahadori <faieze.bahadori@metropolia.fi>
2024-11-11 15:49:13 +0200

    upd

---
+++ moduuli1/javascript/teht.6.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const answer = confirm('Should I calculate the square root?');
+let result;
+
+if (answer === true) {
+    const number = (prompt("Enter a number: "));
+
+    if (number < 0) {
+        result = "The square root of a negative number is not defined";
+    } else (number > 0)
+    {
+        result = "The square root is " + Math.sqrt(number);
+    }}
+else{
+    result = "The square root is not calculated."
+    }
+document.querySelector(".T5").innerHTML = result;
